bcc3401b5379922ee2bc0ca92cd0ea410b4a3944
Jennifer <jenniferlombardo@hotmail.com>
2022-01-17 10:01:02 -0300

    hola

--- PI-Countries-main/client/src/App.js
+++ PI-Countries-main/client/src/App.js
@@ -1,11 +1,24 @@
-import './App.css';
+import './App.css'
+//import Countries from "./components/Countries/countries"
 
-function App() {
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+
+import LandingPage from './components/LandingPage/landingPage.jsx'
+import Home from './components/Home/home.jsx'
+import CountryDetails from './components/Details/details'
+import CreateActivity from './components/Activities/activity'
+
+export default function App () {
   return (
-    <div className="App">
-      <h1>Henry Countries</h1>
-    </div>
-  );
+    <BrowserRouter>
+      <div className='App'>
+        <Routes>
+          <Route path='/activity/' element={<CreateActivity />}></Route>
+          <Route path='/home/:id' element={<CountryDetails />}></Route>
+          <Route path='/home' element={<Home />}></Route>
+          <Route path='/' element={<LandingPage />}></Route>
+        </Routes>
+      </div>
+    </BrowserRouter>
+  )
 }
-
-export default App;
